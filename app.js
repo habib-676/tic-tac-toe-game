eee6f8259a85bbfc98caf880fdd694e7198bcb14
habib-676 <rahman.habibur2421k@gmail.com>
2025-03-19 14:44:47 +0600

    winner message showing correctly

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let boxes = document.querySelectorAll(".box-btn");
 let resetBtn = document.getElementById("reset-btn");
+const msgContainer = document.getElementById("winner-msg-container");
 
 let turn0 = true;
 
@@ -31,15 +32,40 @@ boxes.forEach((box) => {
 
 const checkWinner = () => {
   for (let pattern of winPatterns) {
-    
     let pos1Val = boxes[pattern[0]].innerText;
     let pos2Val = boxes[pattern[1]].innerText;
     let pos3Val = boxes[pattern[2]].innerText;
 
     if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
       if (pos1Val === pos2Val && pos2Val === pos3Val) {
-        console.log("winner", pos1Val);
+        showWinner(pos1Val);
       }
     }
   }
 };
+
+const showWinner = (winner) => {
+  for (let box of boxes) {
+    box.innerText = "";
+  }
+  msgContainer.innerHTML = `
+        <p class="text-center text-3xl font-bold mb-5">Congratulation !! The Winner is ${winner}</p>
+        <button onclick =" newGame()" class="btn btn-secondary h-12 text-xl">
+          New game
+        </button>
+    `;
+  document.getElementById("game-container").classList.add("hidden");
+  resetBtn.classList.add("hidden");
+};
+
+// new game
+
+// document.getElementById("new-game-btn").addEventListener("click", () => {});
+function newGame() {
+  for (let box of boxes) {
+    box.disabled = false;
+  }
+  document.getElementById("game-container").classList.remove("hidden");
+  resetBtn.classList.remove("hidden");
+  msgContainer.innerHTML = "";
+}
